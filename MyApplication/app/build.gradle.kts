8ed5147715ecb3abe896e4c3c22adc4e9f3ee047
yuzhe <1837374190@qq.com>
2026-03-18 09:35:39 +0800

    修改版本号为v1.2.1

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
