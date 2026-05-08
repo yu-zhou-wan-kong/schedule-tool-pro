6a90e86a45b8bab5862953a1e9729992b42bcb53
yuzhe <1837374190@qq.com>
2026-05-08 08:39:32 +0800

    V1.5.0：1、新增功能：可自定义不同班次的背景色和字体色。\n2、调整了“今天”按钮的位置以及大小，放在日历底部，方便单手操作。\n3、优化了“设置”页面，将不常用的设置内容折叠起来，需要时可以展开。\n（提示：更新时需要开启“安装未知应用”的权限，不强制，不想更新可以选择“忽略此版本”）\n已知问题：无法实现更新后自动删除安装包

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.myapplication"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "1.4.1"
+        versionCode = 25
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
